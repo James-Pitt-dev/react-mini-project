c377d2e3891e697c64a7a008898e82b9971715f7
James Pitt <39842510+James-Pitt-dev@users.noreply.github.com>
2026-06-03 16:13:50 -0700

    first template

---
+++ src/components/Rating.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+const Rating = () => {
+
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+
+  const stars = Array.from({length: 5}, (_, i) => i + 1)
+
+  return ( 
+  <div className="rating-container">
+    <h2>
+      Rate Your Experience
+    </h2>
+    <div className="stars">
+      {
+        stars.map((star) => (
+          <span 
+          key={star} 
+          onClick={() => setRating(star)} 
+          onMouseEnter={() => setHover(star)}
+          onMouseLeave={() => setHover(0)}
+          className="star">
+            {'\u2605'}
+          </span>
+        ))
+      }
+    </div>
+  </div> 
+  );
+}
+
+// const styles = {
+//   container: {
+//     textAlign: 'center',
+//     fontFamily: 'Arial',
+//     padding: '20px'
+//   },
+//   heading: {
+//     color: 'red'
+//   }
+// }
+export default Rating
