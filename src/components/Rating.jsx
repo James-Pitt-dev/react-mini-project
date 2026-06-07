7fd18be61b26ff7424c35b4f608cc3253d4f86fe
James Pitt <39842510+James-Pitt-dev@users.noreply.github.com>
2026-06-07 14:47:53 -0700

    submit/close modal

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,32 +1,64 @@
 import { useState } from "react";
+import Star from "./Star";
 
-const Rating = () => {
+const Rating = ({heading = 'Rate Your Experience?', color = 'gold', feedbackMessages = ['Terrible', 'Poor', 'Fair', 'Good', 'Excellent']}) => {
 
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
+  const [submitted, setSubmitted] = useState(false);
 
 
   const stars = Array.from({length: 5}, (_, i) => i + 1)
 
+  const handleSubmit = () => {
+    if (rating>0) {
+      setSubmitted(true)
+    }
+  }
+
+  const closeModal = () => {
+    setSubmitted(false);
+    setRating(0);
+  }
+
   return ( 
   <div className="rating-container">
     <h2>
-      Rate Your Experience
+      {heading}
     </h2>
     <div className="stars">
       {
         stars.map((star) => (
-          <span 
-          key={star} 
-          onClick={() => setRating(star)} 
-          onMouseEnter={() => setHover(star)}
-          onMouseLeave={() => setHover(0)}
-          className="star">
-            {'\u2605'}
-          </span>
+          <Star 
+            key={star} 
+            star={star}
+            rating={rating}
+            hover={hover}
+            color={color}
+            ratingClick={setRating}
+            hoverEnter={() => setHover(star)}
+            hoverLeave={() => setHover(null)}
+          />
         ))
       }
     </div>
+    {rating > 0 && <p className="feedback">{feedbackMessages[rating - 1]}</p> }
+    <button className="submit-btn" onClick={handleSubmit} disabled={rating === 0}>Submit</button>
+
+    {/* {modal} */}
+    {
+      submitted === true ? (
+        <div className="modal-overlay">
+          <div className="modal">
+            <h2>Thank You</h2>
+            <p>You rated us {rating} star{rating > 1 ? 's' : ''}</p>
+            <div className="close-btn" onClick={closeModal}>
+              Close
+            </div>
+          </div>
+        </div>
+      ) : null
+    }
   </div> 
   );
 }
---
+++ src/components/Star.jsx
@@ -0,0 +1,16 @@
+const Star = ({star, rating, hover, color, ratingClick, hoverEnter, hoverLeave}) => {
+    return ( 
+    <span 
+    className="star"
+    style={{
+    color: star <= (hover || rating) ? color : '#ccc' 
+    }}
+    onClick={() => ratingClick(star)}
+    onMouseEnter={hoverEnter}
+    onMouseLeave={hoverLeave}
+    >
+        {'\u2605'}
+    </span> );
+}
+ 
+export default Star;
